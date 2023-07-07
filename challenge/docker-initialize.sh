f6e77836ba2a86945c213351fa1d1f6e67cf3027
Elijah Sippel <37519236+ecuber@users.noreply.github.com>
2023-07-07 21:03:43 +0000

    troubleshooting more

--- challenge/docker-initialize.sh
+++ challenge/docker-initialize.sh
@@ -15,21 +15,25 @@ whoami
 # # Enable binfmt_misc https://access.redhat.com/solutions/1985633
 # sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 echo "mounting"
-sudo mount -t binfmt_misc binfmt_misc /proc/sys/fs/binfmt_misc
-
+# sudo mount -t binfmt_misc binfmt_misc /proc/sys/fs/binfmt_misc
+sudo mount -t binfmt_misc none /proc/sys/fs/binfmt_misc
 # echo 1 > /proc/sys/fs/binfmt_misc/status
 echo "modprbe"
 sudo modprobe binfmt_misc
 
 echo "update-binfmts"
 sudo update-binfmts --enable qemu-aarch64
+sudo update-binfmts --enable qemu-arm
 
-echo "cp"
-sudo cp /usr/bin/qemu-aarch64-static /usr/bin/qemu-aarch64
+# echo "cp"
+# sudo cp /usr/bin/qemu-aarch64-static /usr/bin/qemu-aarch64
+# sudo cp /usr/bin/qemu-arm-static /usr/bin/qemu-aarch64
 
-echo "tee"
-echo ':qemu-aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/usr/bin/qemu-aarch64:' | sudo tee /proc/sys/fs/binfmt_misc/register
-
+# echo "tee"
+# echo ':qemu-aarch64:m::\x7felf\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/usr/bin/qemu-aarch64:' > /proc/sys/fs/binfmt_misc/register
+echo "echo"
+sudo touch /proc/sys/fs/binfmt_misc/register
+sudo echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:' > /proc/sys/fs/binfmt_misc/register
 # cat /proc/sys/fs/binfmt_misc/qemu-aarch64
 
 
