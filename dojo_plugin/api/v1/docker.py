bccd660ded13b01c998c916bbbabe6a498e58d53
Elijah Sippel <37519236+ecuber@users.noreply.github.com>
2023-07-07 19:13:44 +0000

    troubleshooting

--- dojo_plugin/api/v1/docker.py
+++ dojo_plugin/api/v1/docker.py
@@ -95,6 +95,11 @@ def start_challenge(user, dojo_challenge, practice):
                     "bind",
                     propagation="shared",
                 ),
+                docker.types.Mount(
+                    "/proc/sys/fs/binfmt_misc",
+                    "/proc/sys/fs/binfmt_misc",
+                    "bind"
+                )
             ],
             devices=devices,
             network=None if internet else "user_firewall",
