a8014615d07e8ea5bcd7774cd5a984942d7f2076
Elijah Sippel <37519236+ecuber@users.noreply.github.com>
2023-07-24 16:50:07 -0400

    Move insmod to docker-init

--- challenge/docker-initialize.sh
+++ challenge/docker-initialize.sh
@@ -3,35 +3,5 @@
 # send stdout/stderr to logfile
 exec >.initlog 2>&1
 
-# # Add entry to /etc/fstab
-# echo 'binfmt_misc /proc/sys/fs/binfmt_misc binfmt_misc non 0 0' >> /etc/fstab
-
-# # Remount all filesystems
-# sudo mount -a
-
-# echo "whoami"
-# whoami
-
-# # Enable binfmt_misc https://access.redhat.com/solutions/1985633
-# sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
-# echo "mounting"
-# # sudo mount -t binfmt_misc binfmt_misc /proc/sys/fs/binfmt_misc
-# sudo mount -t binfmt_misc none /proc/sys/fs/binfmt_misc
-# # echo 1 > /proc/sys/fs/binfmt_misc/status
-# echo "modprbe"
-# sudo modprobe binfmt_misc
-
-# echo "update-binfmts"
-# sudo update-binfmts --enable qemu-aarch64
-# sudo update-binfmts --enable qemu-arm
-
-# # echo "cp"
-# # sudo cp /usr/bin/qemu-aarch64-static /usr/bin/qemu-aarch64
-# # sudo cp /usr/bin/qemu-arm-static /usr/bin/qemu-aarch64
-
-# # echo "tee"
-# # echo ':qemu-aarch64:m::\x7felf\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/usr/bin/qemu-aarch64:' > /proc/sys/fs/binfmt_misc/register
-# echo "echo"
-# sudo touch /proc/sys/fs/binfmt_misc/register
-# sudo echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:' > /proc/sys/fs/binfmt_misc/register
-# cat /proc/sys/fs/binfmt_misc/qemu-aarch64
+# Add binfmt kernel module
+sudo insmod /lib/modules/$(uname -r)/kernel/fs/binfmt_misc.ko
